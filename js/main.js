82de4284089cbb31479a4119ae75da0ffe015a5a
dannycyph3r <47256908+dannycyph3r@users.noreply.github.com>
2019-04-18 17:26:48 -0400

    problem with the scripts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,55 @@
+// const ALL = 'ALL';
+// const ROCK = 'ROCK';
+// const JAZZ = 'JAZZ';
+// const TRAP = 'TRAP';
+// const CHILL = 'CHILL';
+
+
+
+// let timeline = [
+//   { name: `I love that new album from Eminem`, cat: ALL },
+//   { name: `Work out muis all day`, cat: ROCK },
+//   { name: `Jazz music is life`, cat: JAZZ },
+//   { name: `Dancing every day`, cat: TRAP },
+//   { name: `TRAP TRAP TRAP`, cat: TRAP },
+//   { name: `Chill and study`, cat: CHILL },
+// ];
+
+
+// function printList() {
+//   document.getElementById("timeline").innerHTML = timeline.map(item=>`<li>
+//           <div class="avatar">
+//             <img src="img\226 - Copy.jpg">
+//             <div class="hover">
+//             </div>
+//           </div>
+//           <div class="bubble-container">
+//             <div class="bubble">
+//               <h3>@carlf</h3> <br/>
+//             ${item.name}
+//             </div>
+//             </div>
+//         </li>`)
+// } ;
+ 
+
+function newElement() {
+  
+  let inputValue = document.getElementById("newEkko").value;
+  
+  if (inputValue === '') {
+    alert("Maybe Your keyboard isnt working! lol");
+  } 
+	else {
+    
+    myList.push({ name: inputValue, cat: ALL });
+    printList();
+  }
+
+ }
+ 
+
+
 function myFunction() {
     var x = document.getElementById("dropdown-content");
     if (x.style.display === "block") {
@@ -19,3 +71,4 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
+printList();
